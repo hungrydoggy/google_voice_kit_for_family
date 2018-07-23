dd494a3aa5d846165f2a61c052db5a8c479c1f62
john kim <jhoney510@gmail.com>
2018-07-23 17:01:48 +0900

    update run.py

--- code.py
+++ code.py
@@ -1,9 +1,10 @@
 
+
+print("def")
+sleep(1)
 ledOn()
 
 sleep(1)
-
 ledOff()
 
-sleep(1)
 
--- run.py
+++ run.py
@@ -1,11 +1,17 @@
 #-*- coding: utf-8 -*-
 from module.fam import *
+import sys
+
+if len(sys.argv) >= 2:
+    code_path = sys.argv[1]
+else:
+    code_path = "code.py"
 
 init()
 
 try:
     #execfile('code.py')
-    exec(open("code.py").read())
+    exec(open(code_path).read())
 except Exception as e:
     shutdown()
     raise e
